e32e663c001d273d58c8798920ca20bb50e6f406
jtloong <joshua.t.loong@gmail.com>
2018-12-09 19:55:02 -0500

    Fixing merge conflicts

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import json
 import convert_html
 from bs4 import BeautifulSoup
 
@@ -9,22 +10,26 @@ def find_path(name, path):
         if name in dirs:
             return os.path.join(root, name)
 
-def get_post_folders(year_folders):
+def get_post_folders(post_parent_folders, static_folder):
 	folder_names = []
 	folder_paths = []
 
-	for year in year_folders:
-		walk_results = [x[1] for x in os.walk("website\\" + year) if x[1] != []]
+	for directory in post_parent_folders:
+		walk_results = [x[1] for x in os.walk(static_folder + '\\' + directory) if x[1] != []]
 		folder_names.extend([s[0] for s in walk_results if not s[0].isdigit()])
-		folder_paths.extend([find_path(s[0], "website\\" + year) for s in walk_results if not s[0].isdigit()])
-
+		folder_paths.extend([find_path(s[0], static_folder + "\\" + directory) for s in walk_results if not s[0].isdigit()])
 
 	return folder_paths, folder_names
 
-year_folders = [item for item in os.listdir("website/") if item[:2] == '20']
+# year_folders = [item for item in os.listdir("website/") if item[:2] == '20']
+
+with open('locations.json') as json_data:
+    d = json.load(json_data)
+    static_folder = d['static_folder']
+    post_parent_folders = d['post_folders']
 
 # Traverse directories two levels down
-folder_paths, folder_names  = get_post_folders(year_folders)
+folder_paths, folder_names  = get_post_folders(post_parent_folders, static_folder)
 
 if os.path.exists('posts\\'):
 	shutil.rmtree("posts\\")
