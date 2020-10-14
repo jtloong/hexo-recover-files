20d09ee035feca54e2c8512b57cf73256a7e1af2
ecrax <zugecraftet@gmail.com>
2020-10-14 17:27:40 +0200

    Fixes for Pyhton 3.7

--- convert_html.py
+++ convert_html.py
@@ -4,59 +4,68 @@ from bs4 import BeautifulSoup
 import codecs
 import re
 
+
 def build_file(name):
-	path = "posts/" + name + "/index.html"
+    path = "posts/" + name + "/index.html"
 
-	f = codecs.open(path, 'r')
-	html = f.read()
+    # Added "encoding" parameter, else it would return null
+    f = codecs.open(path, "r", encoding="utf-8")
 
-	soup = BeautifulSoup(html, features="html5lib")
+    html = f.read()
 
-	# Save title
-	md = "---"
-	md += "\ntitle: " + soup.find("h1").get_text().strip()
-	md += "\ndate: " +  soup.find("time").get_text().replace("-", "/")
-	md += "\ntags: " + soup.find("a", "tag-link").get_text()
-	md += "\n---\n"
+    soup = BeautifulSoup(html, features="html5lib")
 
-	content = soup.find(itemprop="articleBody")
+    # Save title
+    md = "---"
+    md += "\ntitle: " + soup.find("h1").get_text().strip()
+    md += "\ndate: " + soup.find("time").get_text().replace("-", "/")
 
-	for child in content.children:
-		if str(child)[:4] == '<img':
-			end_point = child['src'].rfind('/') + 1
-			file_name = child['src'][end_point:]
-			text = "{% asset_img " + file_name + " %}"
-			md += "\n" + text + "\n"
-		elif str(child)[:2] == '<h':
-			num_pounds = "#" * int(str(child)[2])
-			md += "\n" + num_pounds + " " + child.get_text() + "\n"
-		elif str(child)[:3] == '<if':
-			md += "\n" + str(child) + "\n"
-		elif str(child)[:24] == '<figure class="highlight':
-			code_sample = str(child)
+    # This line caused me some trouble, because in my html files
+    # the tags were not displayed.
+    #  md += "\ntags: " + soup.find("a", "tag-link").get_text()
 
-			code_type = code_sample[25: code_sample.find('"', 24)]
+    md += "\n---\n"
 
-			temp_md = Tomd(str(child)).markdown
-			temp_md = temp_md[temp_md.find('<td class="code"'):]
-			temp_md = BeautifulSoup(temp_md, features="html5lib").find("pre")
+    content = soup.find(itemprop="articleBody")
 
-			pre_md = str(temp_md)
-			pre_md = pre_md[5:-6]
-			
-			temp_md = "\n``` "
-			temp_md += code_type + '\n'
-			for i, char in enumerate(pre_md):
-				if pre_md[i:i+5] == '<br/>':
-					temp_md += '\n'
-					temp_md += char
-				else:
-					temp_md += char
-			temp_md += '```' 
+    for child in content.children:
+        if str(child)[:4] == "<img":
+            end_point = child["src"].rfind("/") + 1
+            file_name = child["src"][end_point:]
+            text = "{% asset_img " + file_name + " %}"
+            md += "\n" + text + "\n"
+        elif str(child)[:2] == "<h":
+            num_pounds = "#" * int(str(child)[2])
+            md += "\n" + num_pounds + " " + child.get_text() + "\n"
+        elif str(child)[:3] == "<if":
+            md += "\n" + str(child) + "\n"
+        elif str(child)[:24] == '<figure class="highlight':
+            code_sample = str(child)
 
-			md += temp_md.replace('<br/>', '')
-		else:
-			md += Tomd(str(child)).markdown
+            code_type = code_sample[25 : code_sample.find('"', 24)]
 
-	with open('posts/' + name + '.md', 'w') as file:
-	    file.write(md)
+            temp_md = Tomd(str(child)).markdown
+            temp_md = temp_md[temp_md.find('<td class="code"') :]
+            temp_md = BeautifulSoup(temp_md, features="html5lib").find("pre")
+
+            pre_md = str(temp_md)
+            pre_md = pre_md[5:-6]
+
+            temp_md = "\n``` "
+            temp_md += code_type + "\n"
+            for i, char in enumerate(pre_md):
+                if pre_md[i : i + 5] == "<br/>":
+                    temp_md += "\n"
+                    temp_md += char
+                else:
+                    temp_md += char
+            temp_md += "```"
+
+            md += temp_md.replace("<br/>", "")
+        else:
+            md += Tomd(str(child)).markdown
+
+    # Added "encoding" parameter, else it would throw a UnicodeEncodeError.
+    with open("posts/" + name + ".md", "w", encoding="utf-8") as file:
+        file.write(md)
+
--- extract.py
+++ extract.py
@@ -10,43 +10,53 @@ def find_path(name, path):
         if name in dirs:
             return os.path.join(root, name)
 
+
 def get_post_folders(post_parent_folders, static_folder):
-	folder_names = []
-	folder_paths = []
+    folder_names = []
+    folder_paths = []
 
-	for directory in post_parent_folders:
-		walk_results = [x[1] for x in os.walk(static_folder + '\\' + directory) if x[1] != []]
-		folder_names.extend([s[0] for s in walk_results if not s[0].isdigit()])
-		folder_paths.extend([find_path(s[0], static_folder + "\\" + directory) for s in walk_results if not s[0].isdigit()])
+    for directory in post_parent_folders:
+        walk_results = [
+            x[1] for x in os.walk(static_folder + "\\" + directory) if x[1] != []
+        ]
+        folder_names.extend([s[0] for s in walk_results if not s[0].isdigit()])
+        folder_paths.extend(
+            [
+                find_path(s[0], static_folder + "\\" + directory)
+                for s in walk_results
+                if not s[0].isdigit()
+            ]
+        )
 
-	return folder_paths, folder_names
+    return folder_paths, folder_names
+
 
 # year_folders = [item for item in os.listdir("website/") if item[:2] == '20']
 
-with open('locations.json') as json_data:
+with open("locations.json") as json_data:
     d = json.load(json_data)
-    static_folder = d['static_folder']
-    post_parent_folders = d['post_folders']
+    static_folder = d["static_folder"]
+    post_parent_folders = d["post_folders"]
 
 # Traverse directories two levels down
-folder_paths, folder_names  = get_post_folders(post_parent_folders, static_folder)
+folder_paths, folder_names = get_post_folders(post_parent_folders, static_folder)
 
-if os.path.exists('posts\\'):
-	shutil.rmtree("posts\\")
+if os.path.exists("posts\\"):
+    shutil.rmtree("posts\\")
 
-if not os.path.exists('posts\\'):
-	os.mkdir('posts\\')
+if not os.path.exists("posts\\"):
+    os.mkdir("posts\\")
 
 for index, folder in enumerate(folder_paths):
-	try:
-		shutil.copytree(folder, 'posts\\' + folder_names[index])
-	except:
-		print('Folder already copied.')
+    try:
+        shutil.copytree(folder, "posts\\" + folder_names[index])
+    except:
+        print("Folder already copied.")
 
 for post in folder_names:
-	try:
-		convert_html.build_file(post)
-		print("Built " + post)
-		os.remove("posts/" + post + "/index.html")
-	except:
-		print("No file")
+    try:
+        convert_html.build_file(post)
+        print("Built " + post)
+        os.remove("posts/" + post + "/index.html")
+    except:
+        print("No file")
